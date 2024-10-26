4a181e2ec2f8c29e2e3ecf9f6cdaad7521a58f07
OscarPineda1 <167844008+OscarPineda1@users.noreply.github.com>
2024-10-26 03:32:15 -0500

    Agregando dockerfile

--- obj/Debug/net8.0/App2demo.AssemblyInfo.cs
+++ obj/Debug/net8.0/App2demo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App2demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51d277801753df428c26deb5fed3471c4f24a5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3afc76107ef2d4cc42681554680944c5f0a0225a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App2demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App2demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
