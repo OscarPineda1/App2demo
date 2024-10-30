030637bde7f9f2ffd5d81a10210345b41f66d5c3
OscarPineda1 <167844008+OscarPineda1@users.noreply.github.com>
2024-10-30 11:31:39 -0500

    catalogo

---
+++ Controllers/CatalogoController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App2demo.Data;
+
+namespace App2demo.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CatalogoController> _logger;
+
+        public CatalogoController(ILogger<CatalogoController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var catalogos = from o in _context.DataProducto select o;
+
+            return View(catalogos);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
